377e3acc42156575d96c5244553d7868e9d5f5e8
Unknown <mark_christian_sanchez@dlsu.edu.ph>
2018-08-19 22:51:27 +0800

    Merge branch 'dev'

--- static/js/home.js
+++ static/js/home.js
@@ -1,16 +1,24 @@
 ( function($) {
     // we can now rely on $ within the safety of our "bodyguard" function
     $(document).ready(function(){ 
-        $('#target').teletype({
-            text: [
-                'A CTPRJMN final requirement'
-            ]
-          });
+        // $('#target').teletype({
+        //     text: [
+        //         'A CTPRJMN final requirement'
+        //     ]
+        //   });
           
-          $('#cursor').teletype({
-            text: ['.', ' '],
-            delay: 0,
-            pause: 500
-          });
+        //   $('#cursor').teletype({
+        //     text: ['.', ' '],
+        //     delay: 0,
+        //     pause: 500
+        //   });
+        new TypeIt('#subtext', {
+            strings: ["A CTPRJMN final requirement", "by Mark Christian Sanchez"],
+            speed: 100,
+            breakLines: false,
+            nextStringDelay: 3000
+        })
+
     } );
+    
 } ) ( jQuery );
