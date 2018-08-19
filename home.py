20c3c74e0111627d4b2c3a23fad2cdf194540285
Unknown <mark_christian_sanchez@dlsu.edu.ph>
2018-08-19 23:01:17 +0800

    Fix url linking

--- home.py
+++ home.py
@@ -14,6 +14,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('home.html')
+@app.route('/main')
+def index():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
